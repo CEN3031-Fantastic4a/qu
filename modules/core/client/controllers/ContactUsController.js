c0658602c2856adba0457705d3cd4ce58732f21b
Unknown <leanys@ufl.edu>
2017-10-24 11:30:51 -0400

    Contact us page

---
+++ modules/core/client/controllers/ContactUsController.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+  angular
+  .module('users')
+  .controller('ContactUsController', ContactUsController);
+
+  ContactUsController.$inject = ['$scope', 'Authentication'];
+  function ContactUsController($scope, Authentication) {
+    var vm = this;
+    vm.user = Authentication.user;
+
+    $scope.checkboxModel = {
+      anonymous : true
+    };
+  }
+}());
