90da204b56bba1eab3819e8a2c554bbcf5c41ef2
Allison Kast <allisonakast@gmail.com>
2017-11-12 19:52:49 -0500

    css to the front page
    wew finally finished css to front page
    
    Update core.css
    Improve mess in the CSS
    
    Rewrote the section as needed;
    Got rid of !important in css.
    Need more work
    Improve the CSS
    
    Rewrote the section as needed;
    Got rid of !important in css.
    Need more work
    Last update of the CSS
    
    added hidden overflow to html and couple more percentage width;
    Need to take car of smaller resolutions
    Last update of the CSS
    
    added hidden overflow to html and couple more percentage width;
    Need to take car of smaller resolutions
    fix(css)
    
    error order

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -8,8 +8,6 @@
   function HomeController() {
     var vm = this;
 
-    vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAJEoMA7rbgnOKG2ILkLNSaj8XB9zaR3Bo';
-
     var g = $(function () {
       $('#datetimepicker1').datetimepicker({
         collapse: false
