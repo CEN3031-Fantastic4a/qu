cc8d29096cd441b7ee05a4a4f49d884c0f586e42
Allison Kast <allisonakast@gmail.com>
2017-11-27 17:58:13 -0500

    fix error

--- modules/chat/client/config/chat.client.menus.js
+++ modules/chat/client/config/chat.client.menus.js
@@ -6,12 +6,13 @@
     .run(menuConfig);
 
   menuConfig.$inject = ['menuService'];
-/*
+
   function menuConfig(menuService) {
     // Set top bar menu items
+    /*
     menuService.addMenuItem('topbar', {
       title: 'Chat',
       state: 'chat'
-    });
-  }*/
+    });*/
+  }
 }());
