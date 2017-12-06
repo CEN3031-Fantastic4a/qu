8641a5f24f7a1f9c7b104e427f632be042429c0e
Charles Elvira <ninou14fr@ufl.edu>
2017-12-06 03:30:59 -0500

    home view improvement (#60)
    added disabled and/ or not clickable button if vm.selectedcity is empty 
    
    syntax error fixed
    book now does the payment and will do booking as well in next release
    
    fix styling using fix can be better using col and row properly.
    manage banking hide and show payment
    
    thank shelby For your idea

--- modules/users/client/controllers/settings/customer-banking.controller.js
+++ modules/users/client/controllers/settings/customer-banking.controller.js
@@ -10,6 +10,14 @@
   function customerBankingSetting($scope, $http, $location, UsersService, Authentication, Notification, TokenService) {
     var vm = this;
     vm.user = Authentication.user;
+    $scope.vis = true;
+    $scope.changevis = function () {
+      if ($scope.vis) {
+        $scope.vis = false;
+      } else {
+        $scope.vis = true;
+      }
+    };
     vm.token = TokenService.get(function (data) {
       var x = document.getElementById('drop').getAttribute('value');
       var button = document.querySelector('#submit-button');
